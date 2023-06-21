f5e1949b9c03a812b71d30c5f91c338b579cf0b1
Arpit-Jirapure <124046569+Arpit-Jirapure@users.noreply.github.com>
2023-06-21 15:56:50 +0530

    Create Task-3.2.js

---
+++ Task-3.2.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import ColorPicker from './ColorPicker';
+
+const App = () => {
+  const colors = ['#ff0000', '#00ff00', '#0000ff'];
+
+  return (
+    <div>
+      <h1>Color Picker</h1>
+      <ColorPicker colors={colors} />
+    </div>
+  );
+};
+
+export default App;
