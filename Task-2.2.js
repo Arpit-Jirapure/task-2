44dfe5d82d265394b84977af077cf601add0617e
Arpit-Jirapure <124046569+Arpit-Jirapure@users.noreply.github.com>
2023-06-21 15:54:01 +0530

    Create Task-2.2.js

---
+++ Task-2.2.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import AgeCalculator from './AgeCalculator';
+
+const App = () => {
+  return (
+    <div>
+      <h1>My App</h1>
+      <AgeCalculator />
+    </div>
+  );
+};
+
+export default App;
